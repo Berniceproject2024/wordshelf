0f267958bb5a79fea517e53cf6cbf51426e4a647
Bernice Ahuoiza <ojobernicepraise08@gmail.com>
2026-07-22 22:20:31 +0100

    added new features

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,20 +5,26 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     const [language, setLanguage] = useState()
+    const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
     <div className='nav'>
         <div className='title'><img src='/assets/bread.webp' alt='' className='moviebox-img'/>
          <h1>FAITH SHELF</h1>  </div>
-         <div className='nav-list'>
-            <Link to={home}><p>HOME</p></Link>
-           <Link to={bookcatalogue}><p>BOOKS</p></Link>
-            <Link to={categories}><p>CATEGORIES</p></Link>
-            <Link to={author}><p>AUTHORS</p></Link>
-            <Link to={about}><p>ABOUT</p></Link>
-         </div>
-         <div className='select-option'>
+          <button
+                className="hamburger"
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                {menuOpen ? "✕" : "☰"}
+            </button>
+         <div className={`nav-list ${menuOpen ? "active" : ""}`}>
+            <Link to={home} onClick={() => setMenuOpen(false)}><p>HOME</p></Link>
+           <Link to={bookcatalogue} onClick={() => setMenuOpen(false)}><p>BOOKS</p></Link>
+            <Link to={categories} onClick={() => setMenuOpen(false)}><p>CATEGORIES</p></Link>
+            <Link to={author} onClick={() => setMenuOpen(false)}><p>AUTHORS</p></Link>
+            <Link to={about} onClick={() => setMenuOpen(false)}><p>ABOUT</p></Link>
+         
              <select
       value={language}
       onChange={(e) => setLanguage(e.target.value)}
